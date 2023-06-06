095e0e4f98b871b667573ff8eadc9ca8c8982520
sourav biswas <siswasb@amazon.com>
2023-06-06 16:02:10 -0600

    check all source documents

--- migration/data-differ/data-differ.py
+++ migration/data-differ/data-differ.py
@@ -11,22 +11,22 @@ def connect_to_db(uri, pool_size):
 
 ## Compare doc count, if not equal try to find the missing docs
 ## Needs optimisation for DocumentDB
-def compare_document_counts(srcCollection, tgtCollection, output_file):
-    count1 = srcCollection.count_documents({})
-    count2 = tgtCollection.count_documents({})
+def compare_document_counts(srcCollection, tgtCollection, output_file,src_count,trg_count):
+    print("Compare doc count, if not equal try to find the missing docs")
 
-    if count1 == 0:
+
+    if src_count == 0:
         print("No documents found in the source collection, please re-check you selected the right source collection.")
         return
-    elif count2 == 0:
+    elif trg_count == 0:
         print("No documents found in the target collection, please re-check you selected the right target collection.")
         return
-    elif count1 > count2:
-        print(f"Document count mismatch: Found {count1} documents in the source collection vs. {count2} documents in the target collection, scanning...")
+    elif src_count > trg_count:
+        print(f"Document count mismatch: Found {src_count} documents in the source collection vs. {trg_count} documents in the target collection, scanning...")
         missing_docs = srcCollection.find({'_id': {'$nin': tgtCollection.distinct('_id')}})
         write_difference_to_file(output_file, "Document _IDs present in the source collection but not in target collection:")
     else:
-        print(f"Document count mismatch: Found {count1} documents in the source collection vs. {count2} documents in the target collection, scanning...")
+        print(f"Document count mismatch: Found {src_count} documents in the source collection vs. {trg_count} documents in the target collection, scanning...")
         missing_docs = tgtCollection.find({'_id': {'$nin': srcCollection.distinct('_id')}})
         write_difference_to_file(output_file, "Document _IDs present in the target collection but not in the source collection:")
 
@@ -34,49 +34,78 @@ def compare_document_counts(srcCollection, tgtCollection, output_file):
         print(doc['_id'])
         write_difference_to_file(output_file, doc['_id'])
 
+def compare_document_counts_both_ways(srcCollection, tgtCollection, output_file,src_count,trg_count):
+    print("Compare doc count, if not equal try to find the missing docs")
+
+
+    if src_count == 0:
+        print("No documents found in the source collection, please re-check you selected the right source collection.")
+        return
+    elif trg_count == 0:
+        print("No documents found in the target collection, please re-check you selected the right target collection.")
+        return
+
+    
+    missing_docs = srcCollection.find({'_id': {'$nin': tgtCollection.distinct('_id')}})
+    if len(list(missing_docs.clone())) > 0 :
+        print(f"Document count mismatch: Found {src_count} documents in the source collection vs. {trg_count} documents in the target collection, scanning...")
+        write_difference_to_file(output_file, "Document _IDs present in the source collection but not in target collection:")
+        for doc in missing_docs:
+            print(doc['_id'])
+            write_difference_to_file(output_file, doc['_id'])
+    
+    missing_docs = tgtCollection.find({'_id': {'$nin': srcCollection.distinct('_id')}})
+    if len(list(missing_docs.clone()))  > 0 :
+        print(f"Document count mismatch: Found {src_count} documents in the source collection vs. {trg_count} documents in the target collection, scanning...")
+        write_difference_to_file(output_file, "Document _IDs present in the target collection but not in the source collection:")
+
+        for doc in missing_docs:
+            print(doc['_id'])
+            write_difference_to_file(output_file, doc['_id'])
 
 def compare_docs_deepdiff(doc1, doc2, output_file):
+    #print("doing DeepDiff")
     diff = DeepDiff(doc1, doc2, verbose_level=2, report_repetition=True, ignore_order=True)
     if diff:
-        print("Difference found at doc id", doc1["_id"])
+        print("Difference found at doc id :", doc1["_id"])
         print(diff)
         write_difference_to_file(output_file, doc1["_id"])
         write_difference_to_file(output_file, diff)
 
 
 ## Compare document data, open a cursor for both collections, stream batches of data based on batch_size
-def compare_document_data(srcCollection, tgtCollection, batch_size, output_file):
+def compare_document_data(srcCollection, tgtCollection, batch_size, output_file,src_count,trg_count):
+    print("Compare document data, open a cursor for both collections, stream batches of data based on batch_size")
     cursor1 = srcCollection.find().sort('_id').batch_size(batch_size)
-    cursor2 = tgtCollection.find().sort('_id').batch_size(batch_size)
-
-    total_docs = min(srcCollection.count_documents({}), tgtCollection.count_documents({}))
+    #cursor2 = tgtCollection.find().sort('_id').batch_size(batch_size)
+    total_docs = src_count
     progress_bar = tqdm(total=total_docs, desc='Comparing documents', unit='doc')
 
     ## Pool size based on CPU count
     pool_size = os.cpu_count()
-
+    print(pool_size)
     with Pool(pool_size) as pool:
-        while cursor1.alive or cursor2.alive:
+        while cursor1.alive :
             batch1 = [next(cursor1, None) for _ in range(batch_size)]
-            batch2 = [next(cursor2, None) for _ in range(batch_size)]
-
-            # Remove any None values from the batches
-            batch1 = [doc for doc in batch1 if doc is not None]
-            batch2 = [doc for doc in batch2 if doc is not None]
-
-            # Zip the batches together
-            doc_pairs = zip(batch1, batch2)
-
+            doc_pairs=[]
+            for document in batch1:
+                source_id = document['_id']
+                queried_doc = tgtCollection.find_one({"_id": document["_id"]})
+                if queried_doc == None:
+                    print("source document not found in target :",source_id)
+                    continue
+                else :
+                    doc_pairs.append((document,queried_doc))
+                #compare_docs_deepdiff(document,queried_doc,output_file)
             # Use the pool of worker processes to compare each document pair
             pool.starmap(compare_docs_deepdiff, [(doc1, doc2, output_file) for doc1, doc2 in doc_pairs])
-
-            progress_bar.update(len(batch1))
-
+            progress_bar.update(batch_size)
     progress_bar.close()
 
 
 # Compare indexes between the two collections
 def compare_indexes(srcCollection, tgtCollection, output_file):
+    print("Compare indexes between the two collections")
     src_indexes = srcCollection.index_information()
     tgt_indexes = tgtCollection.index_information()
 
@@ -94,15 +123,12 @@ def write_difference_to_file(output_file, content):
 
 
 def compare_collections(srcCollection, tgtCollection, batch_size, output_file):
-    count1 = srcCollection.count_documents({})
-    count2 = tgtCollection.count_documents({})
+    src_count = srcCollection.count_documents({})
+    trg_count = tgtCollection.count_documents({})
 
     compare_indexes(srcCollection, tgtCollection, output_file)
-
-    if count1 != count2:
-        compare_document_counts(srcCollection, tgtCollection, output_file)
-    else:
-        compare_document_data(srcCollection, tgtCollection, batch_size, output_file)
+    compare_document_counts_both_ways(srcCollection, tgtCollection, output_file,src_count,trg_count)
+    compare_document_data(srcCollection, tgtCollection, batch_size, output_file,src_count,trg_count)
 
 
 def main():
