4e8d5ba861952a612fc80715bef30d2e279d55d3
Mihai Aldoiu <mihaialdoiu@users.noreply.github.com>
2023-10-18 17:38:17 +0100

    Update docdb-stat tool (#89)
    Co-authored-by: Mihai Aldoiu <maldoiu@amazon.com>

--- monitoring/docdb-stat/docdbstat.py
+++ monitoring/docdb-stat/docdbstat.py
@@ -7,7 +7,15 @@ import pandas as pd
 
 
 def connect_to_docdb(uri):
-    # Check for replicaSet in uri and replace with directConnection
+    """Connects to a DocumentDB instance.
+    Check for replicaSet in uri and replace with directConnection.
+
+    Args:
+     uri: The DocumentDB connection URI.
+
+    Returns:
+     A pymongo.MongoClient object.
+    """
     if "replicaSet=rs0" in uri:
         uri = uri.replace("replicaSet=rs0", "directConnection=true")
 
@@ -17,10 +25,25 @@ def connect_to_docdb(uri):
 
 
 def get_server_stats(db):
+    """Retrieve the serverStatus() for a DocumentDB instance.
+    Args:
+     db: A pymongo.MongoClient object.
+
+    Returns:
+     A dictionary containing the server stats.
+    """
     return db.command('serverStatus')
 
 
 def get_replica_status(db):
+    """Retrieve the replica status for a DocumentDB instance.
+
+    Args:
+     db: A pymongo.MongoClient object.
+
+    Returns:
+     The replica status, string.
+    """
     is_master_result = db.command('isMaster')
     if 'setName' in is_master_result:
         if is_master_result['ismaster']:
@@ -29,22 +52,37 @@ def get_replica_status(db):
             return 'Secondary'
 
 
-def display_server_stats(stats, header_interval_counter, db, fields):
-    # Extract specific metrics from the stats dictionary
+def display_server_stats(previous_stats, current_stats, header_interval_counter, db, fields, polling_interval):
+    """Displays db.serverStatus() for a DocumentDB instance.
+
+    Args:
+      previous_stats: The previous stats, or None if there are no previous stats.
+      current_stats: The current stats.
+      header_interval_counter: A counter that is used to determine when to print the header of the table.
+      db: A pymongo.MongoClient object.
+      fields: A list of the fields to display in the table.
+      polling_interval: The polling interval in seconds.
+
+    Returns:
+      previous_stats: dict, current_stats: dict, header_interval_counter: int, db: object, fields: List[str], polling_interval: int
+    """
+    if previous_stats is None:
+        return
+
     metrics = {
-        'Host': stats['host'].split('.')[0],
+        'Host': current_stats['host'].split('.')[0],
         'Status': get_replica_status(db),
-        'Connections': stats['connections']['current'],
-        'Inserts': stats['opcounters']['insert'],
-        'Query': stats['opcounters']['query'],
-        'Updates': stats['opcounters']['update'],
-        'Deletes': stats['opcounters']['delete'],
-        'GetMore': stats['opcounters']['getmore'],
-        'Command': stats['opcounters']['command'],
-        'CursorsTotal': stats['metrics']['cursor']['open']['total'],
-        'CursorsNoTimeout': stats['metrics']['cursor']['open']['noTimeout'],
-        'Transactions': stats['transactions']['currentActive'],
-        'Timestamp': stats['localTime']
+        'Connections': current_stats['connections']['current'],
+        'Inserts': (current_stats['opcounters']['insert'] - previous_stats['opcounters']['insert']) / polling_interval,
+        'Query': (current_stats['opcounters']['query'] - previous_stats['opcounters']['query']) / polling_interval,
+        'Updates': (current_stats['opcounters']['update'] - previous_stats['opcounters']['update']) / polling_interval,
+        'Deletes': (current_stats['opcounters']['delete'] - previous_stats['opcounters']['delete']) / polling_interval,
+        'GetMore': (current_stats['opcounters']['getmore'] - previous_stats['opcounters']['getmore']) / polling_interval,
+        'Command': (current_stats['opcounters']['command'] - previous_stats['opcounters']['command']) / polling_interval,
+        'CursorsTotal': current_stats['metrics']['cursor']['open']['total'],
+        'CursorsNoTimeout': current_stats['metrics']['cursor']['open']['noTimeout'],
+        'Transactions': current_stats['transactions']['currentActive'],
+        'Timestamp': current_stats['localTime']
     }
 
     fields = [field.lower() for field in fields]
@@ -73,11 +111,7 @@ def main(uri, polling_interval, header_interval, fields):
         while True:
             server_stats = get_server_stats(db)
             if previous_server_stats is not None:
-                if header_interval_counter == 0:
-                    display_server_stats(server_stats, header_interval_counter, db, fields)
-                else:
-                    display_server_stats(server_stats, header_interval, db, fields)
-
+                display_server_stats(previous_server_stats, server_stats, header_interval_counter, db, fields, polling_interval)
                 header_interval_counter = (header_interval_counter + 1) % header_interval
 
             previous_server_stats = server_stats.copy()
