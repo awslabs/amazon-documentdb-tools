14de3cd69a8adad1d45559f9bba92a597270cb45
Tim Callaghan <tmcallaghan@gmail.com>
2024-12-30 12:59:12 -0500

    Update create-docdb-dashboard.py

--- monitoring/docdb-dashboarder/create-docdb-dashboard.py
+++ monitoring/docdb-dashboarder/create-docdb-dashboard.py
@@ -79,6 +79,8 @@ def create_dashboard(widgets, region, instanceList, clusterList):
         tempWidgets.append(widget)
         dashboardY += thisRow["height"]
 
+    return tempWidgets
+
 
 # Main method
 def main():
