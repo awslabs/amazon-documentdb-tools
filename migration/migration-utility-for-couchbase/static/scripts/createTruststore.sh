f010325086c6700f789b8827a79bb8d1c59a5aad
Douglas Bonser <dbonser@amazon.com>
2025-10-27 17:55:18 -0500

    added r8g instance types

--- migration/migration-utility-for-couchbase/static/scripts/createTruststore.sh
+++ migration/migration-utility-for-couchbase/static/scripts/createTruststore.sh
@@ -6,7 +6,7 @@ awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_aft
 
 for CERT in rds-ca-*; do
   alias=$(openssl x509 -noout -text -in $CERT | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
-  echo "Importing $alias" >> createTruststore.log
+  echo "Importing $alias" >> setup.log
   keytool -import -file ${CERT} -alias "${alias}" -storepass ${storepassword} -keystore ${truststore} -noprompt
   rm $CERT
 done
@@ -16,5 +16,5 @@ echo "Trust store content is: "
 keytool -list -v -keystore "$truststore" -storepass ${storepassword} | grep Alias | cut -d " " -f3- | while read alias
 do
    expiry=`keytool -list -v -keystore "$truststore" -storepass ${storepassword} -alias "${alias}" | grep Valid | perl -ne 'if(/until: (.*?)\n/) { print "$1\n"; }'`
-   echo " Certificate ${alias} expires in '$expiry'" >> createTruststore.log
+   echo " Certificate ${alias} expires in '$expiry'" >> setup.log
 done
